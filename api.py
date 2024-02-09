3a58878f309471a99764ed46f078facec140155c
plezhs <pramasns@gmail.com>
2024-02-10 08:40:21 +0900

    main

---
+++ api.py
@@ -0,0 +1,185 @@
+api_key = "RGAPI-62c1d244-a9ea-4ef0-ac45-61866e05d062"
+api_url1 = "https://kr.api.riotgames.com/val/status/v1/platform-data"
+
+import aiohttp, json, requests
+from fastapi import FastAPI
+import os, sys
+import asyncio
+from riot_auth import RiotAuth, auth_exceptions
+from getpass import getpass
+import discord
+import random
+import asyncio
+import os
+from discord.ext import commands
+from discord.utils import get
+from hashlib import new
+from requests import session as sesh, get
+from requests.adapters import HTTPAdapter
+from ssl import PROTOCOL_TLSv1_2
+from urllib3 import PoolManager
+from collections import OrderedDict
+from re import compile
+import os
+
+cw = os.get_terminal_size().columns
+global a
+global nm
+global url1
+global url2
+global url3
+global url4
+
+async def Auth(username,password):
+    global a
+    
+    build = requests.get('https://valorant-api.com/v1/version').json()['data']['riotClientBuild']
+    print('Valorant Build '+build)
+
+    RiotAuth.RIOT_CLIENT_USER_AGENT = build + '%s (Windows;10;;Professional, x64)'
+
+    if sys.platform == "win32":
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+    CREDS = username,password
+
+    auth = RiotAuth()
+    try: 
+        await auth.authorize(*CREDS)
+    except auth_exceptions.RiotAuthenticationError:
+        print('Error: Auth Failed, Please check credentials and try again.')
+
+    except auth_exceptions.RiotMultifactorError:
+        print('Accounts with MultiFactor enabled are not supported at this time.')
+    return auth
+async def store(username,password,region):
+  global a
+  global nm
+  global url1
+  global url2
+  global url3
+  global url4
+
+  auth = await Auth(username,password)
+  
+  token_type = auth.token_type
+  access_token = auth.access_token
+  entitlements_token = auth.entitlements_token
+  user_id = auth.user_id
+
+  conn = aiohttp.TCPConnector()
+  session = aiohttp.ClientSession(connector=conn)
+
+  headers = {
+   'X-Riot-Entitlements-JWT' : entitlements_token,
+   'Authorization': 'Bearer '+ access_token,
+  }
+  headersa = {
+   'Authorization': 'Bearer '+ access_token,
+  }
+
+  async with session.get('https://pd.'+region+'.a.pvp.net/store/v1/offers/', headers=headers) as r:
+    pricedata = await r.json()
+  
+  async with session.get('https://pd.'+ region +'.a.pvp.net/store/v2/storefront/'+ user_id, headers=headers) as r:
+    data = json.loads(await r.text())
+  allstore = data.get('SkinsPanelLayout')
+  singleitems = allstore["SingleItemOffers"]
+  skin1uuid = singleitems[0]
+  skin2uuid = singleitems[1]
+  skin3uuid = singleitems[2]
+  skin4uuid = singleitems[3]
+
+  async with session.get(f'https://pd.{region}.a.pvp.net/personalization/v2/players/{user_id}/playerloadout', headers=headers) as r:
+    data = json.loads(await r.text())
+  player = data.get('Identity')
+  playercard = player['PlayerCardID']
+
+  temp = []
+  async with session.post('https://auth.riotgames.com/userinfo', headers=headersa, json={}) as r:
+      data = await r.json()
+  userid = data['acct']['game_name'] #+"#"+ data['acct']['tag_line']
+
+  async with session.get('https://valorant-api.com/v1/weapons/skinlevels/'+ skin1uuid) as r:
+    skin1 = json.loads(await r.text())['data']['displayName']
+    skin1url = json.loads(await r.text())['data']['displayIcon']
+
+  async with session.get('https://valorant-api.com/v1/weapons/skinlevels/'+ skin2uuid) as r:
+    skin2 = json.loads(await r.text())['data']['displayName']
+    skin2url = json.loads(await r.text())['data']['displayIcon']
+
+  async with session.get('https://valorant-api.com/v1/weapons/skinlevels/'+ skin3uuid) as r:
+    skin3 = json.loads(await r.text())['data']['displayName']
+    skin3url = json.loads(await r.text())['data']['displayIcon']
+
+  async with session.get('https://valorant-api.com/v1/weapons/skinlevels/'+ skin4uuid) as r:
+    skin4 = json.loads(await r.text())['data']['displayName']
+    skin4url = json.loads(await r.text())['data']['displayIcon']
+  url1 = skin1url
+  url2 = skin2url
+  url3 = skin3url
+  url4 = skin4url
+
+  async with session.get(f' https://valorant-api.com/v1/playercards/{playercard}') as r:
+    playercardurl = json.loads(await r.text())['data']['displayIcon']
+  def getprice(uuid):
+    for item in pricedata['Offers']:
+      if item["OfferID"] == uuid:
+        return item['Cost']['85ad13f7-3d1b-5128-9eb2-7cd8ee0b5741']
+  
+  def nightmarket(datad):
+    out = []
+    try:
+      for item in datad["BonusStore"]["BonusStoreOffers"]:
+        r = requests.get(f'https://valorant-api.com/v1/weapons/skinlevels/'+item['Offer']['Rewards'][0]['ItemID'])
+        skin = r.json()
+        data = {
+          'name':skin['data']['displayName'],
+          'uuid': item['Offer']['OfferID'],
+          'price': {
+            'oringinal':item['Offer']['Cost']['85ad13f7-3d1b-5128-9eb2-7cd8ee0b5741'],
+            'discount': item['DiscountPercent'],
+            'final': item['DiscountCosts']['85ad13f7-3d1b-5128-9eb2-7cd8ee0b5741'],
+          }
+        }
+        out.append(data)
+      return out
+    except KeyError:
+      return None
+  nm = nightmarket(data)
+  
+  a=list()
+  a.append([skin1,getprice(skin1uuid)])
+  a.append([skin2,getprice(skin1uuid)])
+  a.append([skin3,getprice(skin1uuid)])
+  a.append([skin4,getprice(skin1uuid)])
+  a.append(playercardurl)
+  a.append(userid)
+
+  await session.close()
+
+def re():
+   return a
+def nightm():
+   global nm
+   if nm != None:
+    nm_items = []
+
+    for item in nm:
+      nmitem_text = f"{item['name']} for {item['price']['final']} ({item['price']['oringinal']} with {item['price']['discount']}% discount) \n"
+      nm_items.append(nmitem_text)
+    result = '----------------------------------------------------------\n'.join(nm_items)
+    return result
+
+def url1():
+  global url1
+  return url1
+def url2():
+  global url2
+  return url2
+def url3():
+  global url3
+  return url3
+def url4():
+  global url4
+  return url4
---
+++ main.py
@@ -0,0 +1,145 @@
+import aiohttp, json, requests
+from fastapi import FastAPI
+import os, sys
+import asyncio
+from riot_auth import RiotAuth, auth_exceptions
+from getpass import getpass
+import discord
+import random
+import asyncio
+import os
+from discord.ext import commands
+from discord.utils import get
+from hashlib import new
+from requests import session as sesh, get
+from requests.adapters import HTTPAdapter
+from ssl import PROTOCOL_TLSv1_2
+from urllib3 import PoolManager
+from collections import OrderedDict
+from re import compile
+import os
+import api as m
+
+intents = discord.Intents.default()
+intents.guilds = True
+intents.members = True
+
+
+badwords=[]
+words = []
+
+# path = "./bads"
+# file_lst = os.listdir(path)
+
+# for file in file_lst:
+#     filepath = path + '/' + file
+#     with open(filepath,'r', encoding='UTF8') as bad:
+#         badwords=bad.read().splitlines()
+#         for i in badwords:
+#             words.append(i)
+    
+bot = commands.Bot(command_prefix="!",intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="발로란트 상점"))
+
+def rint(min:int, max:int):
+    max+=1
+    return random.sample(range(min,max),1)
+
+@bot.command(aliases=["valshp","vs","valoshop","valorantshop","vlshop","ㅍ미놰ㅔ","valahop"])
+async def valshop(ctx,a=None,b=None,c=None):
+    if a ==None or b == None or c == None:
+        await ctx.message.delete()
+        await ctx.send(f"{ctx.message.author.mention} 사용법 : !valshop [Riot ID] [Password] [Your Region: na - North America, latam - Latin America, br -	Brazil, eu - Europe, ap - Asia Pacific, kr - Korea]")
+    else:
+        await ctx.message.delete()
+        await asyncio.create_task(m.store(a,b,c))
+        name = ctx.message.author.name
+        name = name.title()
+        embed1 = discord.Embed(timestamp=ctx.message.created_at, colour=discord.Colour.red(),description=m.re()[0][0],title=f"{m.re()[5]}'s\nValorant Shop 1st Offers")
+        embed1.set_image(url=m.url1)
+        embed1.set_thumbnail(url=m.re()[4])
+        embed1.add_field(name="",value=f"Price : {m.re()[0][1]}vp", inline=False) #inline이 False라면 다음줄로 넘깁니다.
+
+        embed2 = discord.Embed(timestamp=ctx.message.created_at, colour=discord.Colour.red(),description=m.re()[1][0],title=f"{m.re()[5]}'s\nValorant Shop 2nd Offers")
+        embed2.set_image(url=m.url2)
+        embed2.set_thumbnail(url=m.re()[4])
+        embed2.add_field(name="",value=f"Price : {m.re()[1][1]}vp", inline=False) #inline이 False라면 다음줄로 넘깁니다.
+
+        embed3 = discord.Embed(timestamp=ctx.message.created_at, colour=discord.Colour.red(),description=m.re()[2][0],title=f"{m.re()[5]}'s\nValorant Shop 3rd Offers")
+        embed3.set_image(url=m.url3)
+        embed3.set_thumbnail(url=m.re()[4])
+        embed3.add_field(name="",value=f"Price : {m.re()[2][1]}vp", inline=False) #inline이 False라면 다음줄로 넘깁니다.
+
+        embed4 = discord.Embed(timestamp=ctx.message.created_at, colour=discord.Colour.red(),description=m.re()[3][0],title=f"{m.re()[5]}'s\nValorant Shop 4th Offers")
+        embed4.set_image(url=m.url4)
+        embed4.set_thumbnail(url=m.re()[4])
+        embed4.add_field(name="",value=f"Price : {m.re()[3][1]}vp", inline=False) #inline이 False라면 다음줄로 넘깁니다.
+        await ctx.send(f"""{ctx.message.author.mention}""")
+        await ctx.send(embeds = [embed1,embed2,embed3,embed4])
+        # await ctx.send(embed = embed1)
+        # await ctx.send(embed = embed2)
+        # await ctx.send(embed = embed3)
+        # await ctx.send(embed = embed4)
+
+@bot.command(aliases=["mn","ㅜㅡ"])
+async def nm(ctx,a=None,b=None,c=None):
+    if a ==None or b == None or c == None:
+        await ctx.message.delete()
+        await ctx.send(f"{ctx.message.author.mention} 사용법 : !nm [Riot ID] [Password] [Your Region: na - North America, latam - Latin America, br -	Brazil, eu - Europe, ap - Asia Pacific, kr - Korea]")
+    else:
+        await ctx.message.delete()
+        await asyncio.create_task(m.store(a,b,c))
+        embed = discord.Embed(timestamp=ctx.message.created_at, colour=discord.Colour.red(), title="Valorant Night Market", description=m.nightm())
+        # embed.set_thumbnail(url="https://prforest.ga/files/img/홍보숲.png")
+        # embed.add_field(name="필트 제목", value="필드 설명", inline=False) #inline이 False라면 다음줄로 넘깁니다.
+        await ctx.send(f"{ctx.message.author.mention}",embed = embed)
+
+
+@bot.command(aliases=[""])
+async def brief(ctx,a=None,b=None,c=None):
+    if a ==None or b == None or c == None:
+        await ctx.message.delete()
+        await ctx.send(f"{ctx.message.author.mention} 사용법 : !nm [Riot ID] [Password] [Your Region: na - North America, latam - Latin America, br -	Brazil, eu - Europe, ap - Asia Pacific, kr - Korea]")
+    else:
+        await ctx.message.delete()
+        await asyncio.create_task(m.store(a,b,c))
+        embed1 = discord.Embed(timestamp=ctx.message.created_at, colour=discord.Colour.red(),description="",title=f"")
+        embed1.set_image(url=m.url1)
+        embed2 = discord.Embed(timestamp=ctx.message.created_at, colour=discord.Colour.red(),description="",title=f"")
+        embed2.set_image(url=m.url2)
+        embed3 = discord.Embed(timestamp=ctx.message.created_at, colour=discord.Colour.red(),description="",title=f"")
+        embed3.set_image(url=m.url3)
+        embed4 = discord.Embed(timestamp=ctx.message.created_at, colour=discord.Colour.red(),description="",title=f"")
+        embed4.set_image(url=m.url4)
+        await ctx.send(f"""{ctx.message.author.mention}""")
+        await ctx.send(embed = embed1)
+        await ctx.send(embed = embed2)
+        await ctx.send(embed = embed3)
+        await ctx.send(embed = embed4)
+
+
+
+
+
+
+@bot.event
+async def on_message(msg):
+    cont = msg.content
+    if msg.author == bot.user:
+        return
+    else:
+        if isinstance(msg.channel, discord.channel.DMChannel):
+            if msg == "!valshop":
+                msg.author.send("a")
+            return
+    # if cont in badwords:
+    #     await msg.channel.send(f'{msg.author.mention} 누가 비속어 사용하래!', reference=msg)
+    await bot.process_commands(msg)
+
+    
+
+bot.run("MTEzNjMxMjQ0NzgyNTg4NzQwNA.G_kaME.xZK6BEjr_Ux91gvIwr-ezhn-Z5RpqGKFQ7xXGM")

de9817995b9cf3d485c1dc5e8920c0977400ebc8
plezhs <pramasns@gmail.com>
2024-02-12 00:19:59 +0900

    logger timer edit

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ time = datetime.datetime.now()
 
 rt =str(time)[0:10]
 with open(f'{rt}.log.txt', 'a') as f:
-    f.write(f"[{time}]--------------------------------------------------\n[{rt}]Bot Started\n[{rt}]--------------------------------------------------\n")
+    f.write(f"[{time}]--------------------------------------------------\n[{time}]Bot Started\n[{time}]--------------------------------------------------\n")
 
 global login
 login = dict()
